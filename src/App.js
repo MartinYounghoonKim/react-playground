2cbeda406292e5e7449a77b3d7424162a880c653
martinyounghoonkim <hoons0131@gmail.com>
2018-10-23 00:57:39 +0900

    Add sample code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import MyComponent from './MyComponent';
+import MyComponent1 from './MyComponent1';
 
 class App extends Component {
+  constructor (props) {
+    super(props);
+    this.state = {
+      value: 0,
+    }
+    console.log('constructor 실행');
+  }
+  componentDidMount () {
+    console.log('componentDidMount 실행');
+    console.log(this.div.getBoundingClientRect().height);
+  }
+
+  handleClick = () => {
+    this.setState({
+      value: this.state.value + 1
+    });
+  };
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <MyComponent1 value={this.state.value}/>
+        {this.state.value < 10 && <MyComponent value={this.state.value}/>}
+        <div ref={ref => this.div = ref }></div>
+        <button onClick={this.handleClick}>클릭</button>
       </div>
     );
   }
